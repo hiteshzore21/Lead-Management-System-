0d293f6e766f35d604bf5ad815faab0e6db9b0d5
hitesh zore <hiteshzore8@gmail.com>
2026-09-04 16:05:03 +0530

    add root api route

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+
 app.use("/api/leads", leadRoutes);
+
 
 app.get("/", (req, res) => {
   res.json({
@@ -18,13 +20,17 @@ app.get("/", (req, res) => {
   });
 });
 
+
+const PORT = process.env.PORT || 5000;
+
+
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
     console.log("MongoDB connected");
 
-    app.listen(process.env.PORT, () => {
-      console.log(`Server running on port ${process.env.PORT}`);
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
     });
   })
   .catch((error) => {
